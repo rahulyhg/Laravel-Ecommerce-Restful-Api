1bade24aafc23781b89a467ccc1e2ae7ef27a331
Moiz Baloch <abdulmoix1@outlook.com>
2018-11-10 18:53:40 +0500

    Model Created with command including -a so its created controller and migration etc

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix'=>'products'], function(){
+	Route::apiResource('/products', 'ProductController');
+	Route::apiResource('/{product}/reviews', 'ReviewController');
+});
