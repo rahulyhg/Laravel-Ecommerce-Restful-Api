45ef6391b08e66739befcb7ea89320149cbf85bb
Moiz Baloch <abdulmoix1@outlook.com>
2018-11-12 22:42:50 +0500

    Product Collection and Resource Transformation is done

---
+++ app/Http/Resources/Product/ProductCollection.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class ProductCollection extends Resource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'discount' => $this->discount,
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price,2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),1) : 'No rating yet',
+            'href' => [
+                'link' => route('products.show',$this->id)
+            ]
+        ];
+    }
+}
